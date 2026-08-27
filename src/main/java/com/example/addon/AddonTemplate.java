6a6f6ebfaa79cf434a4d7078e3301f0e4610b548
ZnKe <172518914+ZnGaming0@users.noreply.github.com>
2026-08-27 09:15:33 +0200

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,35 +1,75 @@
 package com.example.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
 import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.commands.Commands;
+import meteordevelopment.meteorclient.pathing.BaritoneUtils;
 import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.utils.Utils;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 import org.slf4j.Logger;
 
-public class AddonTemplate extends MeteorAddon {
+// Itt már a te saját eléréseid szerepelnek:
+import com.example.addon.commands.LinkCommand;
+import com.example.addon.commands.ShulkerQueueCommand;
+import com.example.addon.hud.ChestCount;
+import com.example.addon.hud.OnlineTSRMembersHUD;
+import com.example.addon.modules.*;
+
+import java.io.File;
+
+public class OmegawareAddons extends MeteorAddon {
+    public static final String MOD_ID = "omegaware-addons";
+    public static ModMetadata MOD_META;
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    
+    // Maradhat a lap neve OmegaWare, de a kód mögötte már a te példád
+    public static final Category CATEGORY = new Category("OmegaWare");
+    public static final HudGroup HUD_GROUP = new HudGroup("OmegaWare");
+
+    public static BetterBaritoneBuild BETTER_BARITONE_BUILD = null;
+
+    public static File GetConfigFile(String key, String filename) {
+        return new File(new File(new File(new File(MeteorClient.FOLDER, "omegaware"), key), Utils.getFileWorldName()), filename);
+    }
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing OmegaWare Addons in com.example.addon package");
 
-        // Modules
-        Modules.get().add(new ModuleExample());
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+            MOD_META = modContainer.getMetadata();
+        });
 
-        // Commands
-        Commands.add(new CommandExample());
+        // Modulok regisztrálása
+        Modules.get().add(new TPAAutomationModule());
+        Modules.get().add(new BeaconRangeModule());
+        Modules.get().add(new ChatFilterModule());
+        Modules.get().add(new ItemFrameDupeModule());
+        Modules.get().add(new BetterStashFinderModule());
 
-        // HUD
-        Hud.get().register(HudExample.INFO);
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            Modules.get().add(new TSRKitBotModule());
+        }
+
+        if (BaritoneUtils.IS_AVAILABLE) {
+            BETTER_BARITONE_BUILD = new BetterBaritoneBuild();
+            Modules.get().add(BETTER_BARITONE_BUILD);
+        }
+
+        // Parancsok
+        Commands.add(new LinkCommand());
+        Commands.add(new ShulkerQueueCommand());
+
+        // HUD elemek
+        Hud.get().register(OnlineTSRMembersHUD.INFO);
+        Hud.get().register(ChestCount.INFO);
     }
 
     @Override
@@ -39,11 +79,12 @@ public class AddonTemplate extends MeteorAddon {
 
     @Override
     public String getPackage() {
+        // Átírva az új package-re
         return "com.example.addon";
     }
 
     @Override
     public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
+        return new GithubRepo("Omega172", "OmegaWare-Addons");
     }
 }
